95581a44dcbb746ab476c5dba651e7a5744806da
Andrea Velasquez <velasquez189@gmail.com>
2018-06-29 19:31:20 -0400

    npm start works

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -3,14 +3,18 @@ const axios = require('axios');
 require('dotenv').load();
 
 router.route('/')
-    .get( (req, res) => {
+    .get((req, res) => {
         console.log(req.query);
         let url;
-        const apiKey = process.env.NYT_API_KEY;
-        req.query.startDate ?
-        url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${req.query.query}&begin_date=${req.query.startDate}`
-            : url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${req.query.query}
-
+        const apiKey = "957f34a759484e6ca435894b3bd5dcc1";
+        !req.query.startDate ?
+            url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${req.query.query}` :
+            req.query.startDate ?
+            url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${req.query.query}&begin_date=${req.query.startDate}` :
+            req.query.startDate && req.query.endDate ?
+            url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${req.query.query}&begin_date=${req.query.startDate}&end_date=${req.query.endDate}` :
+            url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${req.query.query}`;
+            
         axios({
             method: 'get',
             url: url,
